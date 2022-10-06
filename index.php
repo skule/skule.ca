27a806f939ae0baa0d93d08cddbc8cd0b7f71a1d
Youssef-Rachad <69124029+Youssef-Rachad@users.noreply.github.com>
2022-10-06 15:40:50 -0400

    git gud

---
+++ index.php
@@ -0,0 +1,173 @@
+
+n class="section">
+        <div class="container">
+            <!-- Important Links Begin -->
+            <div class="columns is-multiline is-mobile">
+                <!-- The margin is to account for the missing Skulepedia link-->
+                <div style="/*margin-left: 8%;*/" class="column is-2-desktop is-one-third-tablet is-half-mobile has-text-centered major">
+                    <a target='_blank' href="http://courses.skule.ca">
+                        <i class="fa fa-files-o" aria-hidden="true"></i><br>
+                        Past Exams
+                    </a>
+                </div>
+                <div class="column is-2-desktop is-one-third-tablet is-half-mobile has-text-centered major">
+                    <a href="http://skule.ca/calendar">
+                        <i class="fa fa-calendar" aria-hidden="true"></i><br>
+                        Calendar
+                    </a>
+                </div>
+                <div class="column is-2-desktop is-one-third-tablet is-half-mobile has-text-centered major">
+                    <a href="http://skule.ca/campus_resources">
+                        <i class="fa fa-info" aria-hidden="true"></i><br>
+                        Resources
+                    </a>
+                </div>
+                <!--<div class="column is-2-desktop is-one-third-tablet is-half-mobile has-text-centered major">
+                    <a target='_blank' href="http://orientation.skule.ca">
+                        <span class="fa">F!</span><br>
+                        Orientation
+                    </a>
+                </div>-->
+                <div class="column is-2-desktop is-one-third-tablet is-half-mobile has-text-centered major">
+                    <a href="http://skule.ca/affiliated_clubs">
+                        <i class="fa fa-address-book-o" aria-hidden="true"></i><br>
+                        Clubs List
+                    </a>
+                </div>
+                <!-- Skulepedia is not associated so it is no longer in the home page. I'm not deleting this because I'm lazy. Anyone who Inspect Element'd this, please ignore the following div
+                <div class="column is-2-desktop is-one-third-tablet is-half-mobile has-text-centered major">
+                    <a target='_blank' href="http://skulepedia.ca">
+                        <i class="fa fa-book" aria-hidden="true"></i><br>
+                        Skulepedia
+                    </a>
+                </div>
+               
+                <div class="column is-2-desktop is-one-third-tablet is-half-mobile has-text-centered major">
+                    <a target='_blank' href="http://speakup.skule.ca">
+                        <i class="fa fa-comments-o" aria-hidden="true"></i><br>
+                        Course Feedback
+                    </a>
+                </div>
+                <div class="column is-2-desktop is-one-third-tablet is-half-mobile has-text-centered major">
+                    <a target='_blank' href="https://sca.skule.ca">
+                        <i class="fa fa-trophy" aria-hidden="true"></i><br>
+                        Student Choice Awards
+                    </a>
+                </div>
+ -->
+                <div class="column is-2-desktop is-one-third-tablet is-half-mobile has-text-centered major">
+                    <a target='_blank' href="https://skule.ca/academic_representation">
+                        <i class="fa fa-book" aria-hidden="true"></i><br>
+                        Academics
+                    </a>
+                </div>
+                <div class="column is-2-desktop is-one-third-tablet is-half-mobile has-text-centered major">
+                    <a target='_blank' href="https://skule.ca/financial_status">
+                        <i class="fa fa-dollar" aria-hidden="true"></i><br>
+                        Finances
+                    </a>
+                </div>
+            </div>
+            <!-- Important Links End -->
+
+            <hr>
+
+            <!-- Digest Begin -->
+            <div class="content">
+<!-- THE GODDAMN CALENDAR -->
+ <!-- import Full Calendar files -->
+        <link href="assets/js/calendar/fullcalendar.min.css" rel="stylesheet"/>
+        <link href="assets/js/calendar/fullcalendar.print.min.css" rel="stylesheet" media="print"/>
+
+        <!-- Custom CSS -->
+        <link href="assets/js/calendar/calendar.css" rel="stylesheet"/>
+                <div id="calendar" style="margin: 0 10px; font-size:15px;"></div>
+                <style>
+                .fc-center h2 {
+                    font-size: 20px;
+                }
+                </style>
+                <h1>Announcements</h1>
+            </div>
+            <div class="feed columns is-mobile" num="5"></div>
+            <div class="column"><a  class="button is-primary is-medium" href="http://digest.skule.ca" style="white-space: normal; height: auto;">More Announcements at Skule Digest</a></div>
+            <!-- Digest End -->
+        </div>
+    </section>
+    <!-- Main Content End -->
+
+    <!-- About Us -->
+    <section class="section index" id="about">
+        <div class="container">
+            <div class="columns">
+                <div class="column is-6">
+                    <h1 class="title">About Us</h1>
+                    <h1 class="subtitle">Learn more about what the Engineering Society is, what EngSoc does for you, who occupies each role, and how to contact EngSoc. You can also learn more about Skule™, the community of Engineering Students at the University of Toronto.</h1>
+                    <a  class="button is-primary" href="http://skule.ca/about">Learn more</a>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <!-- Get Involved -->
+    <section class="section index" id="get-involved">
+        <div class="container">
+            <div class="columns">
+                <div class="column is-6">
+                    <h1 class="title">Get Involved!</h1>
+                    <h1 class="subtitle">There are so many groups at Skule™ that you can join and be part of! You can learn more about the types of student groups at Skule, as well as contacts for each group. You can also view a calendar of upcoming events at Skule™.</h1>
+                    <a  class="button is-primary" href="http://skule.ca/get_involved">Get Involved!</a>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <!-- Academics + Finances -->
+    <section class="section index" id="finances">
+        <div class="container">
+            <div class="columns is-multiline is-mobile">
+                <div class="column is-6-desktop is-9-mobile">
+                    <h1 class="title">Finances</h1>
+                    <h1 class="subtitle">The Engineering Society helps with financing student projects. You can request funding for Special Projects, receive Cheque Requisitions for past expenses, and apply for Levies from Student fees.</h1>
+                    <a  class="button is-primary" href="http://skule.ca/finances">Learn more</a>
+                </div>
+                <div class="column is-6-desktop is-9-mobile">
+                    <h1 class="title">Academics</h1>
+                    <h1 class="subtitle">The Engineering Society provides many academic services to its Members, such as Academic representation, course descriptions and tips, and a database of past exams and midterms.</h1>
+                    <a  class="button is-primary" href="http://skule.ca/academics">Learn more</a>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <!-- Club Resources -->
+    <section class="section index" id="club-resources">
+        <div class="container">
+            <div class="columns">
+                <div class="column is-6">
+                    <h1 class="title">Club Resources</h1>
+                    <h1 class="subtitle">We provide many resources to EngSoc-affiliated Clubs, groups, and Project Directorships, such as promotion and funding. If you're a student group that would like to affiliate with EngSoc, you can read the affiliation process for Clubs.</h1>
+                    <a  class="button is-primary" href="http://skule.ca/club_resources">View Resources</a>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <!-- Bookings/Rentals + EngSoc Documents -->
+    <section class="section index" id="bookings">
+        <div class="container">
+            <div class="columns">
+                <div class="column is-6">
+                    <h1 class="title">Bookings & Rentals</h1>
+                    <h1 class="subtitle">The Engineering Society provides room bookings on campus and a parking spot booking for EngSoc-affiliated clubs and project directorships, as well as rentals of audio/visual equipment. Additionally, EngSoc rents out lockers for students in certain Engineering buildings.</h1>
+                    <a  class="button is-primary" href="http://skule.ca/bookings">See Bookings and Rentals</a>
+                </div>
+                <div class="column is-6">
+                    <h1 class="title">EngSoc Documents</h1>
+                    <h1 class="subtitle">Read the Engineering Society's governing documents such as our Constitution, Bylaws, and Policies. You can also access a record of Meeting Minutes and other documents from our Board of Directors and Standing Committees, as well as EngSoc's Financial Documents such as budgets.</h1>
+                    <a  class="button is-primary" href="http://skule.ca/documents">View Documents</a>
+                </div>
+            </div>
+        </div>
+    </section>
+}""""""""""""""""""""""""""''""""""""''""""""""''""""""""''""""""""''""'''""""""""""""''""""""""""""""""""""""""''""""""""""
